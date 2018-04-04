5dfc100f3fce50210e25055839a1031409bf05c3
Unknown <caralopezrom@unal.edu.co>
2018-04-04 15:57:55 -0500

    Se permiten parametros a los user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
   ##include DeviseTokenAuth::Concerns::SetAdminByToken
-  ##
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+  end
+  # protected
+  #         def configure_permitted_parameters
+  #             devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :username, :name, :password) }
+              
+  #         end
 end
