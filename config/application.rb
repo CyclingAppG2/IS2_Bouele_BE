f97b589edd6484699cadf8c1092ff32bbfb05fba
Angel David Avila Mesa <adavilam@unal.edu.co>
2018-04-24 03:31:11 +0700

    Reactivated Devise

--- config/application.rb
+++ config/application.rb
@@ -32,11 +32,11 @@ module Bouele
     config.middleware.use ActionDispatch::Flash
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-    #config.to_prepare do
-    #  DeviseTokenAuth::SessionsController.skip_before_action :authenticate_user!
-    #  DeviseTokenAuth::RegistrationsController.skip_before_action :authenticate_user!
-    #  
-    #end
+    config.to_prepare do
+      DeviseTokenAuth::SessionsController.skip_before_action :authenticate_user!
+      DeviseTokenAuth::RegistrationsController.skip_before_action :authenticate_user!
+      
+    end
 
     config.i18n.default_locale = 'es'
     I18n.l Time.now
