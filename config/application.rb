761852b3ca82a0226831a2f8b4667942115ef6db
Unknown <caralopezrom@unal.edu.co>
2018-04-29 14:04:24 -0500

    Agregar patch al header

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,7 @@ module Bouele
         resource '*', 
         :headers => :any, 
         expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-        :methods => [:get, :post, :options, :delete, :put]
+        :methods => [:get, :post, :options, :delete, :put, :patch]
       end
     end
 
