22d20a0e484937b6b51290de0b94a92bfe43f7d9
Unknown <caralopezrom@unal.edu.co>
2018-05-10 18:01:33 -0500

    Correcion serializador minicipality

--- app/serializers/minicipality_serializer.rb
+++ app/serializers/minicipality_serializer.rb
@@ -14,8 +14,8 @@
 #
 
 class MinicipalitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :department_id, :organization_id
+  attributes :id, :name, :department_id
 
   belongs_to :department
-  belongs_to :organization
+
 end
